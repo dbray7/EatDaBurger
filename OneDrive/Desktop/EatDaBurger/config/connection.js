1ec6c0ce49da9785143504886205ec8a33f84125
dbray7 <devenbray@gmail.com>
2020-11-24 20:35:57 -0500

    adding connection.connect to connection.js file

--- OneDrive/Desktop/EatDaBurger/config/connection.js
+++ OneDrive/Desktop/EatDaBurger/config/connection.js
@@ -9,14 +9,16 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+
+
 // Make connection.
-(function (err) {
+connection.connect((function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
-});
+}));
 
 // Export connection for our ORM to use.
 module.exports = connection;
